9a44597a67b3e2f0a0e748f931cb7a964c534894
Roberto Vega <droz.rv@gmail.com>
2025-07-25 16:39:17 -0400

    Dynamic information added.
    Imagelink, title, totalVolume, Floor, etc.
    
    Floor has also been rounded to 2 decimal places.
    
    Arrows added.

--- src/components/home/PopularCollections.jsx
+++ src/components/home/PopularCollections.jsx
@@ -1,33 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleLeft } from '@fortawesome/free-regular-svg-icons';
+import { faCircleRight } from '@fortawesome/free-regular-svg-icons';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/swiper-bundle.css'
+import { Navigation, Pagination, Scrollbar} from 'swiper/modules';
+import axios from "axios";
+
+
 
 export default function PopularCollections() {
+
+  const [info, setInfo] = useState([])
+    const [loading, setLoading] = useState(true);
+  
+    async function fetchApiData() {
+      try {
+        setLoading(true)
+        const { data } = await axios.get("https://remote-internship-api-production.up.railway.app/popularCollections")
+        setInfo(data.data);
+        
+      }
+      catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  
+  
+    useEffect(() => {
+      console.log(info)
+      fetchApiData();
+    }, [])
+  
+    if (loading) {
+      return (
+        <header>
+          <div className="selected-collection">
+            <div className="skeleton-video skeleton" />
+            <div className="selected-collection__description">
+              <div className="skeleton-logo skeleton" />
+              <div className="skeleton-title skeleton" />
+              <div className="skeleton-author skeleton" />
+              <div className="skeleton-details skeleton" />
+              <div className="skeleton-button skeleton" />
+            </div>
+          </div>
+        </header>
+      );
+    }
+
   return (
     <section id="popular-collections">
       <div className="container">
         <div className="row">
           <h2 className="popular-collections__title">Popular Collections</h2>
+          <div className="arrow__swiper__left">
+            <FontAwesomeIcon icon={faCircleLeft} />
+          </div>
+          <div className="arrow__swiper__right">
+            <FontAwesomeIcon icon={faCircleRight} />
+          </div>
           <div className="popular-collections__body">
-            {new Array(6).fill(0).map((_, index) => (
+            {loading ? "loading" : new Array(6).fill(0).map((_, index) => (
               <div className="collection-column popular__collections__column">
                 <Link to="/collection" key={index} className="collection">
                   <img
-                    src="https://i.seadn.io/gcs/files/a5414557ae405cb6233b4e2e4fa1d9e6.jpg?auto=format&dpr=1&w=1920"
+                    src={info[index].imageLink}
                     alt=""
                     className="collection__img"
                   />
                   <div className="collection__info">
-                    <h3 className="collection__name">Bored Ape Kennel Club</h3>
+                    <h3 className="collection__name">{info[index].title}</h3>
                     <div className="collection__stats">
                       <div className="collection__stat">
                         <span className="collection__stat__label">Floor</span>
-                        <span className="collection__stat__data">0.46 ETH</span>
+                        <span className="collection__stat__data">{Number(info[index].floor).toFixed(2)} ETH</span>
                       </div>
                       <div className="collection__stat">
                         <span className="collection__stat__label">
                           Total Volume
                         </span>
-                        <span className="collection__stat__data">281K ETH</span>
+                        <span className="collection__stat__data">{info[index].totalVolume} ETH</span>
                       </div>
                     </div>
                   </div>
