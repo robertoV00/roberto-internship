8d6e74cb3d0eff5c2697eb644ae82fdf12e97c22
Roberto Vega <droz.rv@gmail.com>
2025-08-09 14:33:10 -0400

    Fading animation when user scrolls down has been added.

--- src/components/home/PopularCollections.jsx
+++ src/components/home/PopularCollections.jsx
@@ -13,9 +13,8 @@ import { Navigation, Pagination, Scrollbar} from 'swiper/modules';
 import axios from "axios";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-
-
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function PopularCollections() {
 
@@ -40,7 +39,24 @@ export default function PopularCollections() {
     useEffect(() => {
       console.log(info)
       fetchApiData();
+      
+      // Initialize AOS
+      AOS.init({
+        duration: 1000, // Default duration
+        once: true, // Animation happens only once
+        offset: 200, // Higher offset means animation triggers earlier when scrolling
+        delay: 0, // No global delay
+        easing: 'ease-out', // Smooth easing
+        mirror: false, // Don't repeat animation when scrolling up
+      });
     }, [])
+
+    // Refresh AOS when loading changes
+    useEffect(() => {
+      if (!loading) {
+        AOS.refresh();
+      }
+    }, [loading]);
   
     if (loading) {
       return (
@@ -90,60 +106,64 @@ export default function PopularCollections() {
     <section id="popular-collections">
       <div className="container">
         <div className="row">
-          <h2 className="popular-collections__title">Popular Collections</h2>
+          <h2 className="popular-collections__title" data-aos="fade-up" data-aos-duration="800">
+            Popular Collections
+          </h2>
           <div className="arrow__swiper__left">
             <FontAwesomeIcon icon={faCircleLeft} />
           </div>
           <div className="arrow__swiper__right">
             <FontAwesomeIcon icon={faCircleRight} />
           </div>
-          <Swiper
-                      modules={[Navigation, Scrollbar]}
-                      slidesPerView={6}
-                      slidesPerGroup={1}
-                      spaceBetween={30}
-                      navigation={true}
-                      loop={true}
-                      breakpoints={{
-                        1600: { slidesPerView: 6 },
-                        1200: { slidesPerView: 4 },
-                        1024: { slidesPerView: 3 },
-                        900:  { slidesPerView: 3 },
-                        600:  { slidesPerView: 2 },
-                        0:    { slidesPerView: 1 }
-                      }}
-          >
-            <div className="popular-collections__body">
-              {loading ? "loading" : info.map((_, index) => (
-                <SwiperSlide key={index}>
-                  <div className="collection-column popular__collections__column">
-                      <Link to="/collection" key={index} className="collection">
-                        <img
-                          src={info[index].imageLink}
-                          alt=""
-                          className="collection__img"
-                        />
-                        <div className="collection__info">
-                          <h3 className="collection__name">{info[index].title}</h3>
-                          <div className="collection__stats">
-                            <div className="collection__stat">
-                              <span className="collection__stat__label">Floor</span>
-                              <span className="collection__stat__data">{Number(info[index].floor).toFixed(2)} ETH</span>
-                            </div>
-                            <div className="collection__stat">
-                              <span className="collection__stat__label">
-                                Total Volume
-                              </span>
-                              <span className="collection__stat__data">{info[index].totalVolume} ETH</span>
+          <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+            <Swiper
+                        modules={[Navigation, Scrollbar]}
+                        slidesPerView={6}
+                        slidesPerGroup={1}
+                        spaceBetween={30}
+                        navigation={true}
+                        loop={true}
+                        breakpoints={{
+                          1600: { slidesPerView: 6 },
+                          1200: { slidesPerView: 4 },
+                          1024: { slidesPerView: 3 },
+                          900:  { slidesPerView: 3 },
+                          600:  { slidesPerView: 2 },
+                          0:    { slidesPerView: 1 }
+                        }}
+            >
+              <div className="popular-collections__body">
+                {loading ? "loading" : info.map((_, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="collection-column popular__collections__column">
+                        <Link to="/collection" key={index} className="collection">
+                          <img
+                            src={info[index].imageLink}
+                            alt=""
+                            className="collection__img"
+                          />
+                          <div className="collection__info">
+                            <h3 className="collection__name">{info[index].title}</h3>
+                            <div className="collection__stats">
+                              <div className="collection__stat">
+                                <span className="collection__stat__label">Floor</span>
+                                <span className="collection__stat__data">{Number(info[index].floor).toFixed(2)} ETH</span>
+                              </div>
+                              <div className="collection__stat">
+                                <span className="collection__stat__label">
+                                  Total Volume
+                                </span>
+                                <span className="collection__stat__data">{info[index].totalVolume} ETH</span>
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      </Link>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </div>
-          </Swiper>
+                        </Link>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </div>
+            </Swiper>
+          </div>
         </div>
       </div>
     </section>
